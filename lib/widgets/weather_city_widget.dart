3851f8499104789a12f5dc8add70c51667fc19e2
WSixx <lucas.2007s@gmail.com>
2021-03-25 10:27:59 -0300

    bottom Widget new

--- lib/widgets/weather_city_widget.dart
+++ lib/widgets/weather_city_widget.dart
@@ -17,61 +17,96 @@ class WeatherCityWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
+      child: Column(
+        children: [
+          Center(
+            child: Container(
+              margin: EdgeInsets.all(7),
+              width: 80.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                color: tempColor(),
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      FontAwesomeIcons.thermometerQuarter,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 2,
+                    ),
+                    Text(
+                      weatherData.results.temp.toString(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 3,
+              padding: EdgeInsets.all(40),
+              children: [
+                Container(
+                  width: 100,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white54,
+                      gradient: LinearGradient(
+                          colors: [Colors.red[900], Colors.yellowAccent])),
+                  child: Center(
+                    child: Text(
+                      weatherData.results.sunrise.toString(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 2),
+                  width: 100,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white54,
+                      gradient: LinearGradient(
+                          colors: [Colors.yellowAccent, Colors.red[900]])),
+                  child: Center(
+                    child: Text(
+                      weatherData.results.sunrise.toString(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      /*Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
-                width: 60.0,
-                height: 60.0,
+                width: 100,
+                height: 60,
                 decoration: BoxDecoration(
-                  color: tempColor(),
-                  shape: BoxShape.circle,
-                ),
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        FontAwesomeIcons.thermometerQuarter,
-                        size: 20,
-                      ),
-                      SizedBox(
-                        width: 2,
-                      ),
-                      Text(
-                        weatherData.results.temp.toString(),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                width: 60.0,
-                height: 60.0,
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  shape: BoxShape.circle,
-                ),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white54,
+                    gradient: LinearGradient(
+                        colors: [Colors.red[900], Colors.yellowAccent])),
                 child: Center(
                   child: Text(
                     weatherData.results.sunrise.toString(),
-                  ),
-                ),
-              ),
-              Container(
-                width: 60.0,
-                height: 60.0,
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  shape: BoxShape.circle,
-                ),
-                child: Center(
-                  child: Text(
-                    weatherData.results.sunset.toString(),
                   ),
                 ),
               ),
@@ -79,56 +114,19 @@ class WeatherCityWidget extends StatelessWidget {
           ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
-                width: 60.0,
-                height: 60.0,
+                padding: EdgeInsets.symmetric(horizontal: 2),
+                width: 100,
+                height: 60,
                 decoration: BoxDecoration(
-                  color: tempColor(),
-                  shape: BoxShape.circle,
-                ),
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        FontAwesomeIcons.thermometerQuarter,
-                        size: 20,
-                      ),
-                      SizedBox(
-                        width: 2,
-                      ),
-                      Text(
-                        weatherData.results.temp.toString(),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                width: 60.0,
-                height: 60.0,
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  shape: BoxShape.circle,
-                ),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white54,
+                    gradient: LinearGradient(
+                        colors: [Colors.yellowAccent, Colors.red[900]])),
                 child: Center(
                   child: Text(
                     weatherData.results.sunrise.toString(),
-                  ),
-                ),
-              ),
-              Container(
-                width: 60.0,
-                height: 60.0,
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  shape: BoxShape.circle,
-                ),
-                child: Center(
-                  child: Text(
-                    weatherData.results.sunset.toString(),
                   ),
                 ),
               ),
@@ -192,7 +190,7 @@ class WeatherCityWidget extends StatelessWidget {
             ],
           ),
         ],
-      ),
+      ),*/
     );
   }
 }
